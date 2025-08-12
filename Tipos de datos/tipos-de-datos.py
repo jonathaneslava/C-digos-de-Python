a65924a0bc5dc7fa32a93849451a02bdf825c4ba
unknown <jadrian1112@gmail.com>
2025-08-11 22:49:29 -0600

    Explicacion breve de tipos de datos

---
+++ Tipos de datos/tipos-de-datos.py
@@ -0,0 +1,17 @@
+#Formas validas para decir que es un tipo de texto
+#Se usan para una sola linea de texto
+"String"
+'string'
+#Se usa para varias lineas de texto
+"""string
+    string"""
+'''string
+    string'''
+#---------------------------------------------------------------------------------
+#Tipos de numeros
+40 #Entero int
+40.2 #Flotante float
+#---------------------------------------------------------------------------------
+#Boolean se escribe con mayuscula en python
+True 
+False
