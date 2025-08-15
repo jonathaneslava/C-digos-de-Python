55862725d4aaff6fbef42723ab613ea30e1ca939
unknown <jadrian1112@gmail.com>
2025-08-14 19:17:29 -0600

    Complemento de los ejemplos de metodos cadenas

--- Metodos/metodos_cadenas.py
+++ Metodos/metodos_cadenas.py
@@ -1,6 +1,6 @@
 #Metodos y funciones en python
 cadena1 = "Hola soy Jonathan"
-cadena2 = "Bienvenido"
+cadena2 = "Bienvenido,Jonathan"
 
 #Dir es una funcion de python, devuelve la lista de atributos validos del objeto pasado
 resultado = dir(cadena1)
@@ -38,3 +38,37 @@ print(es_numerico)
 #isalpha, si es alfanumerico devuelve true, en caso de que no entonces devuelve false
 es_alfanumerico = cadena1.isalpha()
 print(es_alfanumerico) #Los espacios no son alfanumericos por eso el resultado es false, en caso de no tener espacios devuelve true
+
+#Buscamos una cadena en otra cadena, devuelve la cantidad de veces que coincida
+#COUNT cuenta las veces que encuentra una cadena en otra cadena que coincida o caracteres que coincidan
+contrar_coinicidencias = cadena1.count("Hola")
+print(contrar_coinicidencias)
+
+#Funcion Len, Contamos cuantos caracteres tiene una cadena
+contar_caracteres = len(cadena1)
+print(contar_caracteres)
+
+#Startswitch, verificamos si una cadena empieza con una cadena dada, si es asi devuelve true
+empieza_con = cadena1.startswith("H")
+print(empieza_con)
+
+#Endswitch, verificamos si una cadena termina con una cadena dada, si es asi devuelve true
+termina_con = cadena1.endswith("Jonathan")
+print(termina_con)
+
+#Replace, remplaza un pedazo de la cadena dada, por otra cadena 
+#Si el valor 1, se encuentra en la cadena original, remplaza el valor 1 de la misma, por el valor 2
+#Si no encuentra coincidencias entonces no modifica nada y regresa el valor original
+cadena_nueva = cadena1.replace("h","J")
+print(cadena_nueva)
+#Sirve para remplazar comas por espacios en una cadena
+cadena_nueva1 = cadena2.replace(","," ")
+print(cadena_nueva1)
+#Sirve para quitar mayusculas en caso de encontrarlas en una cadena
+cadena_nueva2 = cadena_nueva1.capitalize()
+print(cadena_nueva2)
+
+#Split, Separa cadenas con la cadena que le pasemos
+#Si nuestra cadena dice "Hola, Jonathan" nos da 2 elementos en lista porque esta separada por una coma, tenemos dos elementos
+cadena_separada = cadena2.split(",")
+print(cadena_separada)
