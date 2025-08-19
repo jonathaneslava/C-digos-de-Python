aa969065b7f1910e646505b5b61c55b404bab619
unknown <jadrian1112@gmail.com>
2025-08-19 16:16:44 -0600

    Explicacion de creacion de tuplas

---
+++ Variables 2.0/tuplas.py
@@ -0,0 +1,13 @@
+#Creando tuplas con tuple, para ello se coloca el nombre de tuple y dentro recibe una lista con los elementos
+tupla = tuple(["dato1","datos2"])
+print(tupla)
+
+#Otra forma de crear tuplas es colocarlo sin parentesis
+tupla_sin_parentesis = "dato1","dato2"
+print(tupla_sin_parentesis)
+
+#Si queremos crear una tumpla con un solo elemento y sin parentesis
+tupla_de_un_elemento = "dato1",
+print(tupla_de_un_elemento)
+
+#Las tuplas se crean cuando son elementos de solo lectura, se ocupan las listas cuando se quieren modificar los elementos
