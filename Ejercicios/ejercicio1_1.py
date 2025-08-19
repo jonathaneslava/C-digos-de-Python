a4f70413db722966c8186875e1d88c323bb179f1
unknown <jadrian1112@gmail.com>
2025-08-18 22:23:30 -0600

    Ejercicio de porcentaje, calcular los tiempos de los cursos de python en internet

--- Ejercicios/ejercicio1_1.py
+++ Ejercicios/ejercicio1_1.py
@@ -13,4 +13,22 @@ diferencia_con_promedio = 100 - curso_actual / otros_cursos_promedio *100
 print(f"El curso actual dura un {diferencia_con_min}% menos que el curso mas rapido")
 print(f"El curso actual dura un {diferencia_con_max}% menos que el curso mas lento")
 print(f"El curso actual dura un {diferencia_con_promedio}% menos que el curso promedio")
+print("--------------------------------")
+
+#Duracion de videos sin edicion
+sin_edicion_promedio = 5
+sin_edicion_actual = 3.5
+
+#Porcentaje de tiempo vacio
+tiempo_vacio_promedio = 100 - otros_cursos_promedio * 1000 // sin_edicion_promedio / 10
+tiempo_vacio_actual = 100 - curso_actual * 1000 // sin_edicion_actual / 10
+
+#Diferencia de video removido 
+print(f'Un curso promedio elimina un {tiempo_vacio_promedio}% de video vacio')
+print(f'El curso actual elimina un {tiempo_vacio_actual}% de video vacio')
+print("--------------------------------")
+
+#Mostrando diferencia si los cursos duraran 10 horas
+print(f'Ver 10 horas del curso actual equivale a ver {otros_cursos_promedio * 100 // curso_actual / 10} horas de otros cursos')
+print(f'Ver 10 horas de otros cursos equivale a ver {curso_actual * 100 // otros_cursos_promedio / 10} horas de teste curso')
 
