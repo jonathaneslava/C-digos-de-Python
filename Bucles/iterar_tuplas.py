dfe6fc40bab32a6c1426c8798a999b1a2775912b
unknown <jadrian1112@gmail.com>
2025-08-20 20:08:20 -0600

    Correcion en comentarios

--- Bucles/iterar_tuplas.py
+++ Bucles/iterar_tuplas.py
@@ -1,25 +1,25 @@
 #Ejemplo de bucle con for
 
-#Creamos una lista 
+#Creamos una tupla 
 animales = ("gato", "perro", "loro","cocodrilo","pez")
-#Se crea el bucle for para recorrer la lista de animales
-#la variable animal se crea para que tome el valor del elemento de la lista cada que se itere
+#Se crea el bucle for para recorrer la tupla de animales
+#la variable animal se crea para que tome el valor del elemento de la tupla cada que se itere
 for animal in animales:
     print(f'Ahora la variable animal es: {animal}')
     
-#Creamos una nueva lista para iterar pero esta vez con numeros
-#Queremos que cada elemento en la lista sea multiplicado por 2
+#Creamos una nueva tupla para iterar pero esta vez con numeros
+#Queremos que cada elemento en la tupla sea multiplicado por 2
 numeros = (15,20,25,30,35)
 #Se crea el bucle
 for numero in numeros:
-    resultado = numero *2 #La variable numero se multiplica x2 cada que se itera la lista
-    print(f'El numero en la lista es: {resultado}')
+    resultado = numero *2 #La variable numero se multiplica x2 cada que se itera la tupla
+    print(f'El numero en la tupla es: {resultado}')
     
-#Si queremos iterar dos listas al mismo tiempo utilizamos zip
-#Para que esto funcione necesitamos que las listas tengan la misma cantidad de elementos
+#Si queremos iterar dos tuplas al mismo tiempo utilizamos zip
+#Para que esto funcione necesitamos que las tuplas tengan la misma cantidad de elementos
 for animal,numero in zip(animales,numeros):
-    print(f'Iterando la lista de animales: {animal}')
-    print(f'Iterando la lista de numeros: {numero}')
+    print(f'Iterando la tupla de animales: {animal}')
+    print(f'Iterando la tupla de numeros: {numero}')
     
 #Para usar range en un for
 #Los parametros que solicita es donde comienza y donde termina el bucle aunque el ultimo elemento no se muestra solo es un n-1
@@ -28,20 +28,20 @@ for num in range(1,10):
 #Si solo le colocamos 1 parametro va comenzar desde cero hasta el numero colocado
 for num in range (20):
     print(num)
-#Si queremos recorrer una lista utilizando range no es una forma optima pero colocamos
-#Len nos ayuda a saber la cantidad de elementos de la lista entonces como solo recibe un solo valor range
-#Entonces comienza desde cero al tamaño de la lista
-#despues nos muestra los elementos de la lista tomando la variable anima para mostrar cada valor en cada iteracion
+#Si queremos recorrer una tupla utilizando range no es una forma optima pero colocamos
+#Len nos ayuda a saber la cantidad de elementos de la tupla entonces como solo recibe un solo valor range
+#Entonces comienza desde cero al tamaño de la tupla
+#despues nos muestra los elementos de la tupla tomando la variable anima para mostrar cada valor en cada iteracion
 for anima in range(len(animales)):
     print(animales[anima])
     
-#Forma correcta de recorrer una lista con su indice
+#Forma correcta de recorrer una tupla con su indice
 for anima in enumerate(animales): #Nos da una tupla como resultado (indice, valor)
     indice = anima[0] #Colocar anima[0] nos muesta el indice que tiene la tupla
     valor = anima[1] #Colocar anima[1] nos muesta el valor que tiene la tupla
     print(f'el indice es {indice}, el valor es {valor}')
 
-#Usar for/else se ejecuta el else cuando ya se itero toda la lista
+#Usar for/else se ejecuta el else cuando ya se itero toda la tupla
 for numero in numeros:
     print(f'Valor actual del bucle {numero}')
 else:
