9e7cf231d43f26d3c10fd8df711881aedb311214
unknown <jadrian1112@gmail.com>
2025-08-26 22:32:20 -0600

    Operaciones basicas en python

---
+++ Ejercicios/ejercicio2_4.py
@@ -0,0 +1,29 @@
+#Funciones con operaciones en python
+
+#Funcion suma
+def suma(num1,num2):
+    return num1+num2
+
+#Funcion resta
+def resta(num1,num2):
+    return num1-num2
+
+#Funcion multiplicacion
+def multiplicacion(num1,num2):
+    return num1*num2
+
+#Funcion division
+def division(num1,num2):
+    return num1/num2
+
+op_suma = suma(2,3)
+print(op_suma)
+
+op_resta = resta(2,3)
+print(op_resta)
+
+op_multiplicacion = multiplicacion(2,3)
+print(op_multiplicacion)
+
+op_division = division(2,3)
+print(op_division)
