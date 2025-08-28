f2e4e9734795b07cbf4cc60c96f1d30a53df12d3
unknown <jadrian1112@gmail.com>
2025-08-27 22:29:22 -0600

    LLamar las funciones de un modulo que esta en otras carpetas, establecer la ruta

--- Modulos/modulos_2.0.py
+++ Modulos/modulos_2.0.py
@@ -1,5 +1,13 @@
 #Si se importan funciones de otro modulo de importan de la sig forma
 import funciones_buenas.saludo
+#Si deseamos importar desde otras carpetas atras se tiene que revisar la ruta del archivo
+import sys
+#print (sys.path) Esto nos ayuda para saber las rutas en las que se encuentran nuestros archivos
+sys.path.append('c:\\Users\\Jonathan Eslava\\Desktop\\Python\\funciones_de_prueba_en_modulos')
+import funciones_matematicas
 
 #Para llamar al metodo saludar se debe colocar toda la ruta donde se encuentra para poder acceder de forma correcta
 print(funciones_buenas.saludo.saludar("Jonathan"))
+
+#Para llamar al metodo suma entonces colocamos 
+print(funciones_matematicas.suma(2,30))
