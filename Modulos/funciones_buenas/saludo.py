95027b5ec49d7f35fc9a28b767e1cf24c24862d6
unknown <jadrian1112@gmail.com>
2025-08-27 22:14:15 -0600

    Explicacion de como importar funciones de un modulo a otro desde otra carpeta

---
+++ Modulos/funciones_buenas/saludo.py
@@ -0,0 +1,7 @@
+def saludar(nombre):
+    return f'Hola! {nombre}, espero que estes bien'
+
+def saludar_banda(nombre):
+    return f'Hola! {nombre}, Que onda bro'
+
+print(__name__)
---
+++ Modulos/modulos_2.0.py
@@ -0,0 +1,5 @@
+#Si se importan funciones de otro modulo de importan de la sig forma
+import funciones_buenas.saludo
+
+#Para llamar al metodo saludar se debe colocar toda la ruta donde se encuentra para poder acceder de forma correcta
+print(funciones_buenas.saludo.saludar("Jonathan"))
