20b6f197db0682adaef8e59cd680beb31eab6e0a
unknown <jadrian1112@gmail.com>
2025-08-27 22:42:01 -0600

    Explicacion de como importar paquetes

---
+++ Modulos/paquete.py
@@ -0,0 +1,5 @@
+#Para que python detecte que una carpeta es un paquete lo que se tiene que hacer es
+#Crear un archivo con el nombre de "__init__.py" el archivo no debe contener nada python lo visualiza como paquete 
+import paquete
+
+print(type(paquete))
