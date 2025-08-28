398fa1abf6d31987ef7c619fa7c58f0122b9e472
unknown <jadrian1112@gmail.com>
2025-08-27 22:28:24 -0600

    Codigo de ejemplo para llamarlo desde otro archivo en otra carpeta

---
+++ funciones_de_prueba_en_modulos/funciones_matematicas.py
@@ -0,0 +1,17 @@
+#Funciones con operaciones en python
+
+#Funcion suma
+def suma(num1,num2):
+    return num1+num2
+
+#Funcion resta
+def resta(num1,num2):
+    return num1-num2
+
+#Funcion multiplicacion
+def multiplicacion(num1,num2):
+    return num1*num2
+
+#Funcion division
+def division(num1,num2):
+    return num1/num2
