829dac0841457b49754186da68cebd893a498e46
unknown <jadrian1112@gmail.com>
2025-08-28 21:02:45 -0600

    Se quito el incremento manualmente del ciclo for

--- Ejercicios/ejercicio_dibujar_triangulo.py
+++ Ejercicios/ejercicio_dibujar_triangulo.py
@@ -1,9 +1,7 @@
 #Ejercicio de dibujar un triangulo rectangulo
-n=10 #Definimos el limite de base que tendra el triangulo
+n=6 #Definimos el limite de base que tendra el triangulo
 for i in range (1,n): #El ciclo va de i=1 hasta el limite de base de nuestro triangulo
-    i=i+1 #Incrementamos nuestra variable ahora i=2
-    for j in range (1,i): #El segundo ciclo j=1 hasta i dibuja la cantidad de "*" del triangulo
-        j=j+1
+    for j in range (i): #El segundo ciclo j=1 hasta i dibuja la cantidad de "*" del triangulo
         print("*",end="")        
     print()#Realiza el salto de linea para dibujar los nuevos "*" del triangulo
 
