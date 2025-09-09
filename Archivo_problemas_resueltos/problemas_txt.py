a70668b55e7f60cf2674b3852a49f53053235d65
unknown <jadrian1112@gmail.com>
2025-09-08 20:53:58 -0600

    Explicacion del ciclo for para agregar nombres y apellidos en un txt

--- Archivo_problemas_resueltos/problemas_txt.py
+++ Archivo_problemas_resueltos/problemas_txt.py
@@ -3,7 +3,12 @@ nombres = ["Jonathan","Adrian","Raul","Samuel","Jose"]
 apellidos = ["Eslava", "Arroyo", "Martinez", "Hernandez","Rodriguez"]
 
 #Registrar esta informacion en un TXT de forma optima
-with open("nombres_y_apellidos.txt","w") as archivo:
-    archivo.writelines("Los archivos son: \n")
+with open("Archivo_problemas_resueltos\\nombres_y_apellidos.txt","w") as archivo:
+    archivo.writelines("Los archivos son: \n\n")
     #Realizamos un bucle for que agregara los nombres y apellidos en el txt
     [archivo.writelines(f'nombre:{n}\napellido:{a}\n--------------------\n') for n,a in zip(nombres,apellidos)]
+
+#Si queremos crear el for como comunmente se realiza y no en una sola linea entonces colocamos
+#for n,a in zip(nombres, apellidos):
+   #archivo.writelines(f'nombre:{n}\napellido:{a}\n--------------------\n')
+#El for anterior tiene la misma funcionalidad de agregar los nombres y apellidos al archivo txt
