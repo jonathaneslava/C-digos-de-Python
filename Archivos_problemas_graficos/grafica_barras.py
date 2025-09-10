2f36502b5b9e9d7bb4664e71a73f98a2f11d2329
unknown <jadrian1112@gmail.com>
2025-09-09 20:56:09 -0600

    Explicacion de grafica de barras

---
+++ Archivos_problemas_graficos/grafica_barras.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Leemos el archivo csv, colocamos la ruta y se lee
+df = pd.read_csv("Archivos_problemas_graficos\\ingresos.csv")
+#indicamos que realizaremos un grafico de barras con barplot
+#Especificamos los ejes de la grafica, busca la columna fuente y la establece como eje x
+#Busca la columna ingreso y la establece como eje y, finalmente de donde se obtiene la informacion
+#Se coloca df ya que es el documento csv donde estan los datos
+sns.barplot(x="fuente",y="ingreso",data=df)
+
+plt.show() #Con esto mostramos el grafico
