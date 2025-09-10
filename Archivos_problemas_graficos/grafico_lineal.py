55d45911562d33d9e405d7d2c4892fc800310e33
unknown <jadrian1112@gmail.com>
2025-09-09 20:49:02 -0600

    Comentarios adicionales

--- Archivos_problemas_graficos/grafico_lineal.py
+++ Archivos_problemas_graficos/grafico_lineal.py
@@ -11,7 +11,7 @@ df = pd.read_csv("Archivos_problemas_graficos\\peso.csv")
 sns.lineplot(x="fecha",y="peso",data=df)
 
 #Si quiero marcar el punto mas alto de mis datos se coloca:
-#La fecha en la que esta el peso mas alto, despues el peso y finalmente la marca "o" que es un punto 
+#La fecha en la que esta el peso mas alto, despues el peso y finalmente la marca "o" que es un punto
 plt.plot("01-09",72,"o")
 
 plt.show() #Con esto mostramos el grafico
