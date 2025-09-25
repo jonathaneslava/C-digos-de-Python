72657d0b976a16cfe35b72b7b72f26626d8ae722
unknown <jadrian1112@gmail.com>
2025-09-24 20:01:48 -0600

    Explicacion de clases y objetos en python

---
+++ POO/clases_y_objetos.py
@@ -0,0 +1,8 @@
+#Para definir una clase en python se recomienda colocar pascalcase en el nombre
+class Celular():
+    #Atributos estaticos, porque para todos los objetos van a ser iguales
+    marca = "Samsumg" 
+    modelo = "S23"
+    camara = "48MP"
+    
+#Un objeto es una instancia de una clase
